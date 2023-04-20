4b8dca18d326fafc3ee42671e8b66eda1149b925
brunogsiq <brunogsiq@gmail.com>
2023-04-20 00:45:58 -0300

    New folder and project reorganization

---
+++ Cypress_Framework/10_xhr/xhr.js
@@ -0,0 +1,23 @@
+/*Para remover */
+
+/*Inserir no e2e do config*/
+
+hideXHRInCommandLog: true;
+
+/*Inserir no e2e do support*/
+
+if (Cypress.config("hideXHRInCommandLog")) {
+    const app = window.top;
+   
+    if (
+    app &&
+    !app.document.head.querySelector("[data-hide-command-log-request]")
+    ) {
+    const style = app.document.createElement("style");
+    style.innerHTML =
+    ".command-name-request, .command-name-xhr { display: none }";
+    style.setAttribute("data-hide-command-log-request", "");
+   
+    app.document.head.appendChild(style);
+    }
+}
