192a04a057372deb6f751b5a01e3b180b2b1fee0
brunogsiq <brunogsiq@gmail.com>
2023-05-05 01:11:49 -0300

    Add information and folders

--- Cypress_Framework/01_Dependence/05_faker.js
+++ Cypress_Framework/01_Dependence/05_faker.js
@@ -7,19 +7,19 @@ Instalando:
 ++ sobre a biblioteca: https://fakerjs.dev/guide/usage.html
 
 Como utilizar:
+    NodeJS:
     import { faker } from '@faker-js/faker';
 
     const randomName = faker.name.fullName(); // Rowan Nikolaus
     const randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
-    Or if you using CommonJS
 
-    js
+    CommonJS
     const { faker } = require('@faker-js/faker');
 
     const randomName = faker.name.fullName(); // Rowan Nikolaus
     const randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
+
     Browser#
-    html
     <script type="module">
     import { faker } from 'https://cdn.skypack.dev/@faker-js/faker';
 
---
+++ Visual_Studio/Atalhos.cs
@@ -0,0 +1,7 @@
+/*
+    ctrl + shift + f9   : remove todos os brealpoints
+    ctrl + .            : vincula ao código caso não esteja encontrando o esperado
+    ctrl + rr           : renomeia em todos os locais
+    ctrl + r + g        : Limpa???
+    
+*/
