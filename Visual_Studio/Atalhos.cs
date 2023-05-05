dd34a931fb1c35e985843c1c6314e6a2e046f39a
brunogsiq <brunogsiq@gmail.com>
2023-05-05 19:03:50 -0300

    add dicas

--- Visual_Studio/Atalhos.cs
+++ Visual_Studio/Atalhos.cs
@@ -1,7 +1,11 @@
 /*
-    ctrl + shift + f9   : remove todos os brealpoints
-    ctrl + .            : vincula ao código caso não esteja encontrando o esperado
-    ctrl + rr           : renomeia em todos os locais
-    ctrl + r + g        : Limpa???
-    
+    ctrl + shift + f9    : Remove todos os brealpoints
+    ctrl + .             : Vincula ao código caso não esteja encontrando o esperado
+    ctrl + r+r           : Renomeia em todos os locais
+    ctrl + r+g           : Limpa???
+    ctrl + k+c           : Realiza comentário
+    ctrl + k+v           : Remove comentário
+    ctrl + k+d           : Identa código
+    ctrl + r+g           : Remove using´s desnecessárias
+    ctrl + tabtab        : Construção de metódos
 */
